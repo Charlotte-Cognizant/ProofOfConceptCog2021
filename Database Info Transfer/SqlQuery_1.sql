625c098ff141bc076f34e16ce186796d844d8b3e
DHCTS <David.Huang@cognizant.com>
2021-07-06 14:18:26 -0400

    readme

---
+++ Database Info Transfer/SqlQuery_1.sql
@@ -0,0 +1,5 @@
+﻿
+INSERT INTO Footprint_Information (UID,Address,Perimeter,Area,Center_Point_X,Center_Point_Y,Date_Requested)
+VALUES (1,15504,542101,524653,43523.25, 4856251, '2017-06-13')
+
+SELECT * FROM Footprint_Information
