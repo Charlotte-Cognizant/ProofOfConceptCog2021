38fc383f2a4206f7889ca52cef8f2db672f9aaba
Charlotte-Cognizant <charlotte.roux@cognizant.com>
2021-07-12 15:58:45 -0500

    working on multipage application switch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,14 @@
 import { Component } from "@angular/core";
+import { EventEmitter, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy } from "@angular/compiler/src/core";
+@Component(
 
-@Component({templateUrl: 'home.component.html'})
-export class HomeComponent{}
+    {selector: 'home-route',
+    templateUrl: './home.component.html'
+})
+
+export class HomeComponent implements OnInit{
+    constructor(){}
+    ngOnInit(){}
+}
 
