c5db4e0a9302a9d8c79517bfaa770076d4f4ce40
Colton Hart <hartc17@gmail.com>
2021-07-14 14:17:34 -0400

    change python runner

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -12,9 +12,6 @@ using Microsoft.EntityFrameworkCore;
 using System.Drawing;
 using Newtonsoft.Json;
 using System.Net.Http;
-using API.DTO;
-using Newtonsoft.Json.Converters;
-using System.Dynamic;
 
 namespace API.Controllers
 {
@@ -66,6 +63,7 @@ namespace API.Controllers
                 Area = area,
                 center_Lat = Center_Lat,
                 center_Long = Center_Long,
+                imageByte = image,
 
             };
             _context.spatial.Add(spatialinfo);
@@ -74,26 +72,25 @@ namespace API.Controllers
             return spatialinfo;
         }
 
-        public byte[] ImagetoByte ()
+        public byte[] ImagetoByte (string imagePath)
         {
-            string imagePath = "./imagery/";
             FileStream filestream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
             byte[] imageByteArray = new byte[filestream.Length];
 
             filestream.Read(imageByteArray, 0, imageByteArray.Length);
 
+
             return imageByteArray;
         }
 
-        public Image BytetoImage(string uniqueID)
+        public Image BytetoImage(byte[] imageArray)
         {
-
             using (MemoryStream ms = new MemoryStream(imageArray))
             {
                 return Image.FromStream(ms);
             }
         }
-        /*
+
         [HttpGet]
         public static async Task<object> jsonGet()
         {
@@ -111,9 +108,73 @@ namespace API.Controllers
             }
             return null;
         }
-        */
 
-        private void runPythonScript(string cmd, string args){
+        public string notAvailError(){
+            return ("Sorry, the address number you entered is not available at this time.");
+        }
+
+        public string notFoundError(){
+            return ("Sorry, we cannot find the address you entered. Check for errors and try again.");
+
+        }
+
+        public string notDetectedError(){
+            return ("Sorry, we did not detect any buildings at the address entered.");
+        }
+
+
+
+        private void runPythonScript(AddressData address){
+
+            string address_str = String.Format("{0}, {1}, {2} {3}", address.StreetAddress, address.City, address.State, address.Zip);
+
+            // Set working directory and create process
+            var workingDirectory = "C:/Documents/workcode/scripts";
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = workingDirectory
+                }
+            };
+            process.Start();
+            // Pass multiple commands to cmd.exe
+            using (var sw = process.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    // Vital to activate Anaconda
+                    sw.WriteLine("C:\\Users\\hartc\\anaconda3\\Scripts\\activate.bat");
+                    // Activate ox environment
+                    sw.WriteLine("conda activate ox");
+                    // set environment variables and init mapbox api
+                    sw.WriteLine("set MAPBOX_ACCESS_TOKEN=pk.eyJ1IjoiaGFydGMxNyIsImEiOiJja3IyNWxmMGQyODZyMnB0OXJlOHd4ZGJrIn0.2abXKt7EfUNNHWzvj6buRg");
+                    sw.WriteLine("mapbox ...");
+                    // run your script. You can also pass in arguments
+                    sw.WriteLine(string.Format("python script.py '{1}' geojson" , address_str));
+                }
+            }
+            // read multiple output lines
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+                if (line == "Sorry, address number not available at this time"){
+                    notAvailError();
+                }
+                else if (line == "Address cannot be found."){
+                    notFoundError();
+                }
+                else if (line == "No building detected at given address."){
+                    notDetectedError();
+                }
+            }
+
+
+/*          //old code
             ProcessStartInfo start = new ProcessStartInfo();
             start.FileName = "C:/Documents/workcode/scripts";
             start.Arguments= string.Format("{0} {1}" , cmd, args);
@@ -122,20 +183,10 @@ namespace API.Controllers
             using (Process process = Process.Start(start)){
                 using (StreamReader reader = process.StandardOutput){
                     string result = reader.ReadToEnd();
-
-                    dynamic config = JsonConvert.DeserializeObject<ExpandoObject>(result, new ExpandoObjectConverter());
-                    
-                    var spatialinfo = new SpatialInfo {
-                        Area = config.area,
-                        center_Lat = config.Center_Lat,
-                        center_Long = config.Center_Long,
-
-                    };
-                    _context.spatial.Add(spatialinfo);
-                    _context.SaveChangesAsync();
-                    Console.Write (config);
+                    Console.Write (result);
                 }
             }
+            */
         }
 
 
